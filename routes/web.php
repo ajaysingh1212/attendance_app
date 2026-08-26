7c59a3e61c4564d376be6e1946072b972dd50269
ajay kumar <ajaykumarwrs1997@gmail.com>
2026-08-26 13:34:49 +0530

    sac

--- routes/web.php
+++ routes/web.php
@@ -215,7 +215,7 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'mi
     Route::put('employees/{employee}', [EmployeeController::class, 'update'])->name('employees.update');
     Route::delete('employees/{employee}', [EmployeeController::class, 'destroy'])->name('employees.destroy');
     Route::get('employees/{employee}', [EmployeeController::class, 'show'])->name('employees.show');
-    Route::match(['get', 'post'], '/employees/{employee}/download-pdf', [EmployeeController::class, 'downloadPayrollPdf'])
+    Route::match(['get', 'post'], '/employees/{employee}/download-pdf', [EmployeeController::class, 'downloadPdf'])
     ->name('employees.downloadPdf');
     // Route::get('employees/offer-letter', [App\Http\Controllers\Admin\EmployeeController::class, 'offerLetter'])
     // ->name('employees.offer-letter');
@@ -239,8 +239,8 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'mi
     Route::get('payrolls/manual-adjust/{payrollId}', [PayrollController::class, 'manualAdjustmentForm'])->name('payrolls.manualAdjustForm');
     Route::put('payrolls/manual-adjust/{payrollId}', [PayrollController::class, 'manualAdjustmentUpdate'])->name('payrolls.manualAdjust');
 
-Route::get('payrolls/download/{format}', [PayrollController::class, 'downloadPayrollPdf'])
-    ->name('payrolls.download');
+    Route::get('payrolls/download/{format}', [PayrollController::class, 'downloadPayrollPdf'])->name('payrolls.download');
+
 
     Route::get('payroll', [PayrollController::class, 'index'])
         ->name('payroll.index');
@@ -256,8 +256,8 @@ Route::get('payrolls/download/{format}', [PayrollController::class, 'downloadPay
 
     Route::get('employees/{id}/salary-details', [PayrollController::class, 'getSalaryDetails']);
 
-Route::get('payrolls/download/{payrollId}', [PayrollController::class, 'downloadPayrollPdf'])
-    ->name('payrolls.download');
+    Route::get('payrolls/download', [PayrollController::class, 'download'])
+        ->name('payrolls.download');
 
     Route::get('payrolls/details', [PayrollController::class, 'details'])
         ->name('payrolls.details');
