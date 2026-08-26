94c251c5d495753d53caf5cf488e7e1d9ab4eb46
ajay kumar <ajaykumarwrs1997@gmail.com>
2026-08-26 13:27:05 +0530

    cscs

--- routes/web.php
+++ routes/web.php
@@ -239,8 +239,8 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'mi
     Route::get('payrolls/manual-adjust/{payrollId}', [PayrollController::class, 'manualAdjustmentForm'])->name('payrolls.manualAdjustForm');
     Route::put('payrolls/manual-adjust/{payrollId}', [PayrollController::class, 'manualAdjustmentUpdate'])->name('payrolls.manualAdjust');
 
-    Route::get('payrolls/download/{format}', [PayrollController::class, 'downloadPayrollPdf'])->name('payrolls.download');
-
+Route::get('payrolls/download/{format}', [PayrollController::class, 'downloadPayrollPdf'])
+    ->name('payrolls.download');
 
     Route::get('payroll', [PayrollController::class, 'index'])
         ->name('payroll.index');
@@ -256,8 +256,8 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'mi
 
     Route::get('employees/{id}/salary-details', [PayrollController::class, 'getSalaryDetails']);
 
-    Route::get('payrolls/download', [PayrollController::class, 'downloadPayrollPdf'])
-        ->name('payrolls.download');
+Route::get('payrolls/download/{payrollId}', [PayrollController::class, 'downloadPayrollPdf'])
+    ->name('payrolls.download');
 
     Route::get('payrolls/details', [PayrollController::class, 'details'])
         ->name('payrolls.details');
